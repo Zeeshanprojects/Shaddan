a97b25fb73b616dcc520e12ef33b7e1c7f185f29
Zeeshanprojects <zeeshan.khan124@hotmail.com>
2025-10-10 17:05:53 +0500

    top bar updated

---
+++ shaddan/src/Components/Header.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import "./header.css";
+import Images from "../assets/Images/Image";
+export default function Header() {
+  return (
+    <>
+  <div className="top-bar bg-dark py-2">
+  <div className="container d-flex justify-content-between align-items-center">
+    <p className="text-white mb-0">Welcome to Mianoor Textile Industries</p>
+    <p className="text-white mb-0">info@shaddantextile.com</p>
+  </div>
+</div>
+
+
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Shaddan Textile
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+
+              <li className="nav-item dropdown ">
+                <a
+                  className="nav-link dropdown-toggle active"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <button className="btn btn-outline-success" type="submit">
+                Contact
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
---
+++ shaddan/src/assets/Images/Image.jsx
@@ -0,0 +1,4 @@
+const Images={
+    logo:new URL('./shaddan-logo.webp',import.meta.url).href,
+}
+export default Images;
