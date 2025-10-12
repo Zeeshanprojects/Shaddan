5041809e20d5848f2f7e15997c224994e5819a8e
Zeeshanprojects <zeeshan.khan124@hotmail.com>
2025-10-12 17:47:33 +0500

    images addes

--- shaddan/src/assets/Images/Image.jsx
+++ shaddan/src/assets/Images/Image.jsx
@@ -2,5 +2,10 @@ const Images={
     logo:new URL('./Shaddan-Textile-Logo.png',import.meta.url).href,
     banner:new URL("./shaddan-banner.jpg",import.meta.url).href,
 banner2:new URL("./shaddan-banner2.png",import.meta.url).href,
+towels:new URL("./Towel.webp",import.meta.url).href,
+garments:new URL("./Garments.webp",import.meta.url).href,    
+shreetss:new URL("./sheets.webp",import.meta.url).href,
+Bathrobes:new URL("./Bathrobes.webp",import.meta.url).href,
+
 }
 export default Images;
